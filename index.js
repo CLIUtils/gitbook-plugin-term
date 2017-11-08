7d65c2541eccec81375649ae6eb1b8420c30e843
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-11-08 15:18:17 -0500

    Adding highlighter

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 var XRegExp = require('xregexp');
+var path = require('path');
+var hljs = require('highlight.js');
+
+var MAP = {
+    'py': 'python',
+    'js': 'javascript',
+    'json': 'javascript',
+    'rb': 'ruby',
+    'csharp': 'cs',
+};
+
+function normalize(lang) {
+    if(!lang) { return null; }
+
+    var lower = lang.toLowerCase();
+    return MAP[lower] || lower;
+}
+
+function highlight(lang, code) {
+    if(!lang) return {
+        body: code,
+        html: false
+    };
+
+    // Normalize lang
+    lang = normalize(lang);
+
+    try {
+        return hljs.highlight(lang, code).value;
+    } catch(e) { }
+
+    return {
+        body: code,
+        html: false
+    };
+}
 
 function processLine(line, config) {
     var str = "";
@@ -41,15 +77,9 @@ function processLine(line, config) {
     return {"line":str, "command":commands};
 }
 
-function processBlock(block) {
-    var book          = this;
-    /* Make a copy */
-    var orig_config   = Object.assign({}, book.config.get('pluginsConfig.term', {}));
-    /* Merge dictionaries */
-    var config        = Object.assign(orig_config, block.kwargs);
-
+function process(thebody, config, isweb) {
     var str =  `<pre class="term t-${config.style}${config.fade ? ' t-fade' : ''}">`;
-    var body = block.body.trim().split('\n');
+    var body = thebody.trim().split('\n');
     var lines = "";
     var commands = ""
     for(i=0; i<body.length; i++) {
@@ -59,13 +89,24 @@ function processBlock(block) {
     }
 
     /* JSON backend (README) should not have buttons (it does not have js or styles) */
-    var isweb = book.output.name == 'website';
     if(isweb && config.copyButtons && commands != "") {
         str += `<button class="btn t-copy" data-clipboard-text="${commands.trim()}"><svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="14" aria-hidden="true"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg></button>`
     }
 
     str += lines + '</pre>';
-    return str;
+    return {body:str, html:true};
+}
+
+function processBlock(block) {
+    var book          = this;
+    /* Make a copy */
+    var orig_config   = Object.assign({}, book.config.get('pluginsConfig.term', {}));
+    /* Merge dictionaries */
+    var config        = Object.assign(orig_config, block.kwargs);
+
+    var isweb = book.output.name == 'website';
+
+    return process(block.body, config, isweb).body;
 }
 
 module.exports = {
@@ -90,6 +131,18 @@ module.exports = {
   blocks: {
       term: {
           process: processBlock
+      },
+      code: function(block) {
+          var language = normalize(block.kwargs.language);
+
+          if(language && language == "term") {
+              var opts   = this.config.get('pluginsConfig.term');
+              var isweb = this.output.name == 'website';
+
+              return process(block.body, opts, isweb).body;
+          } else
+              return highlight(language, block.body);
       }
+
   }
 };
